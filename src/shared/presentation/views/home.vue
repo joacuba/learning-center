3efc9e5fdd62a9c732b4570c71ee0716a9f0fe5b
joacuba <jqcuba@gmail.com>
2026-09-21 11:34:56 -0500

    feat(home): add home page view component

--- src/shared/presentation/views/home.vue
+++ src/shared/presentation/views/home.vue
@@ -9,14 +9,13 @@ const {t} = useI18n()
   <section class="pt-6 p-4 md:p-5">
     <div class="flex flex-col gap-3">
       <h1 class="text-4xl font-bold text-color">
-        {{ t("about.title") }}
+        {{ t("home.title") }}
       </h1>
       <p class="m-0 line-height-3 text-color-secondary">
-        {{ t("about.content") }}
+        {{ t("home.content") }}
       </p>
     </div>
   </section>
-
 </template>
 
 <style scoped>
